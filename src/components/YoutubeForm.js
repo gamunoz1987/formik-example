adf491b22c3e8d80ed18a067efd2252c143068fd
gabriel <gamunoz1987@gmail.com>
2020-11-29 10:14:03 -0300

    Form control with validation error

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -9,23 +9,61 @@ function YoutubeForm (){
         channel:''
     }
 
+    const onSubmit = values =>{
+        console.log(values);
+    }
+
+    const validate= values =>{
+        let errors ={}
+
+        if (!values.name){
+            errors.name ='Required'
+        }
+
+        if (!values.email){
+            errors.email='Required'
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9]+\.[A-Z]{2,4}$/i.test(values.email)){
+            errors.email='Invalid email format'
+        }
+
+
+        if (!values.channel){
+            errors.channel='Required'
+        }
+
+        return errors;
+    }
+
     const formik = useFormik({
-        initialValues
+        initialValues,
+        onSubmit,
+        validate
     })
 
     return(
         <div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
+
+                <div className='form-control'>
                 <label htmlFor='name'>Name</label>
                 <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name}/>
+                {formik.errors.name ? <div className='error'>{formik.errors.name}</div>  : null}
+                </div>
 
+                <div className='form-control'>
                 <label htmlFor='email'>E-Mail</label>
                 <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email}/>
+                {formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
+                </div>
 
+
+                <div className='form-control'>
                 <label htmlFor='channel'>Channel</label>
                 <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
-            
-                <button>Submit</button>
+                {formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
+                </div>
+
+                <button type='submit'>Submit</button>
             </form>
 
         </div>
