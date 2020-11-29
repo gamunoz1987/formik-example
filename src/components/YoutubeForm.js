071b21b5f3ed20e26faf70e2379b6a87558e6d3d
gabriel <gamunoz1987@gmail.com>
2020-11-29 09:39:11 -0300

    First commit on formik example

---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {useFormik} from 'formik';
+
+function YoutubeForm (){
+
+    const initialValues={
+        name:'',
+        email:'',
+        channel:''
+    }
+
+    const formik = useFormik({
+        initialValues
+    })
+
+    return(
+        <div>
+            <form>
+                <label htmlFor='name'>Name</label>
+                <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name}/>
+
+                <label htmlFor='email'>E-Mail</label>
+                <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email}/>
+
+                <label htmlFor='channel'>Channel</label>
+                <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
+            
+                <button>Submit</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default YoutubeForm;
